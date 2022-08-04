3302ace73218f33c14f8ea103e7e197c0701ad62
AmineHLub <Amino.bhl@gmail.com>
2022-08-04 21:21:40 +0100

    Delete unecessary export, Add buttons only on non-touch screens, Finilize Style

--- src/Components/ImageDiaporama.js
+++ src/Components/ImageDiaporama.js
@@ -1,20 +1,35 @@
-/* eslint-disable no-unused-vars */
+/* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable react/prop-types */
 import { useState } from 'react';
-import handleImageDiaporama from '../tools/handleImageDiaporama';
 
 export default function ImageDiaporama({ data, setCurrentImageIndex, currentImageIndex }) {
   const [initialCursorPosition, setInitialCursorPosition] = useState(0);
   const [finalCursorPosition, setFinalCursorPosition] = useState(0);
 
+  const handleImageDiaporama = (type) => {
+    if (type === 'next') {
+      if (currentImageIndex + 1 < data.length) {
+        setCurrentImageIndex(currentImageIndex + 1);
+      } else {
+        setCurrentImageIndex(0);
+      }
+    } else if (type === 'prev') {
+      if (currentImageIndex - 1 >= 0) {
+        setCurrentImageIndex(currentImageIndex - 1);
+      } else {
+        setCurrentImageIndex(data.length - 1);
+      }
+    }
+  };
+
   const calculateDeltaMvt = () => {
     const deltaMvt = finalCursorPosition - initialCursorPosition;
     // margin of Δ movement tolerance is above 40px
     if (deltaMvt > 0 && deltaMvt > 40) {
-      handleImageDiaporama(currentImageIndex, data.length, 'prev', setCurrentImageIndex);
+      handleImageDiaporama('prev');
     }
     if (deltaMvt <= 0 && deltaMvt < -40) {
-      handleImageDiaporama(currentImageIndex, data.length, 'next', setCurrentImageIndex);
+      handleImageDiaporama('next');
     }
     return '';
   };
@@ -27,10 +42,33 @@ export default function ImageDiaporama({ data, setCurrentImageIndex, currentImag
       onTouchMove={(e) => setFinalCursorPosition(e.targetTouches[0].clientX)}
       onTouchEnd={() => calculateDeltaMvt()}
     >
+      <div
+        className="diaporama-image-button prev"
+      >
+        <img
+          aria-hidden
+          onClick={() => handleImageDiaporama('prev')}
+          src="https://i.imgur.com/68ZQIM2.png"
+          alt="previous-img"
+        />
+      </div>
+
       <img
+        draggable="false"
         src={data[currentImageIndex].url}
         alt={data[currentImageIndex].title}
       />
+
+      <div
+        className="diaporama-image-button next"
+      >
+        <img
+          aria-hidden
+          onClick={() => handleImageDiaporama('next')}
+          src="https://i.imgur.com/evo0I1R.png"
+          alt="next-img"
+        />
+      </div>
     </div>
   );
 }
