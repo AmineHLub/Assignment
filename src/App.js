f22c60bbf4041ebfbca227954c9d78db38be1ee0
AmineHLub <Amino.bhl@gmail.com>
2022-08-04 18:19:55 +0100

    Fetch data from Api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+/* eslint-disable no-unused-vars */
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.scss';
 import Thumbnail from './Components/Thumbnail';
 import ImageDiaporama from './Components/ImageDiaporama';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/photos').then((res) => {
+      setData(res.data.slice(0, 6));
+    }).catch((err) => {
+      console.error(err, 'uncauhgt');
+    });
+  }, []);
+
   return (
     <div className="App">
       <h1>MASTER WiZR Modules</h1>
